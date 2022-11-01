2440fcf860005cf4c105fe9cf5e099863a825e91
0x1eef <0x1eef@protonmail.com>
2022-11-01 08:51:47 -0300

    ThemeSelect.jsx: store theme choice in a cookie
    Fix #1

---
+++ src/js/components/TheQuran/ThemeSelect.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { set as setCookie } from "es-cookie";
+
+type Props = {
+  setTheme: (theme: string) => void,
+  theme: string
+}
+
+export function ThemeSelect({setTheme, theme}: Props) {
+  const onThemeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setCookie("theme", e.target.value, {domain: location.host, expires: 365})
+    setTheme(e.target.value);
+  }
+
+  return (
+    <select name="theme" value={theme} onChange={onThemeChange}>
+      <option value="moon">The Moon 🌛</option>
+      <option value="leaf">The Leaf 🌿</option>
+    </select>
+  );
+}
