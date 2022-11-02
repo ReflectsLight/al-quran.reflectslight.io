7d4974cf5830cc58a0c4905b4040d223975c9c45
0x1eef <0x1eef@protonmail.com>
2022-11-02 00:49:43 -0300

    Ruby: fix linter errors

--- lib/rake/cmd.rb
+++ lib/rake/cmd.rb
@@ -1,14 +1,15 @@
+# frozen_string_literal: true
+
 module Cmd
   def cmd(cmd, *args, silent: false)
     print "  -> #{cmd}: " unless silent
     _, result = Process.wait2 Process.spawn(
       [cmd, cmd],
       *args,
-      STDERR=>File::NULL,
-      STDOUT=>File::NULL
+      $stderr => File::NULL,
+      $stdout => File::NULL
     )
-    case
-    when result.success?
+    if result.success?
       print Paint["OK", :green, :bold], "\n" unless silent
     else
       print Paint["ERROR", :red, :bold], "\n" unless silent
--- lib/tasks/deploy.rb
+++ lib/tasks/deploy.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Tasks::Deploy
   require_relative "deploy/local"
 end
