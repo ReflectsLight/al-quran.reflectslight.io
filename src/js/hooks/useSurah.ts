dfee1d25e286e4af8ae61692a0ce177872c5e41a
0x1eef <0x1eef@protonmail.com>
2022-11-01 22:36:38 -0300

    s/surahByNum/surahId/g

--- src/js/hooks/useSurah.ts
+++ src/js/hooks/useSurah.ts
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
 import { Quran } from "lib/Quran";
 
-export default function(locale: string, surahByNumber: number) {
+export default function(locale: string, surahId: number) {
   const [surah, setSurah] = useState(null);
 
   useEffect(() => {
     (async () => {
-      const res  = await fetch(`/json/${locale}/${surahByNumber}.json`);
+      const res  = await fetch(`/json/${locale}/${surahId}.json`);
       const json = await res.json();
       setSurah(Quran.Surah.fromJSON(json.shift(), json));
     })();
