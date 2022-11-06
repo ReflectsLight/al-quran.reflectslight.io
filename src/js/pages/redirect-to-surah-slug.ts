e7c78c9c628fe82b45497d4786a551547a31c9ef
0x1eef <0x1eef@protonmail.com>
2022-11-06 06:51:28 -0300

    src/: add src/js/pages/redirect-to-surah-slug.ts

---
+++ src/js/pages/redirect-to-surah-slug.ts
@@ -0,0 +1,9 @@
+(function() {
+  const [locale, surahId] = location.pathname
+                                    .split("/")
+                                    .filter(function(s) { return s.length })
+                                    .slice(-2);
+  const el: HTMLElement = document.querySelector(".surah-id-to-slug");
+  const slugs = JSON.parse(el.innerText);
+  location.replace(["", locale, slugs[surahId]].join("/"));
+})();
