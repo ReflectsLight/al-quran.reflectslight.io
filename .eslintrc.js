5e2ab86e2243a0d75bfd678099180584193a25ce
Gyen Abubakar <indehyde@gmail.com>
2022-11-11 16:11:34 +0000

    eslint & prettier setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-  extends: ['standard-with-typescript', 'standard-jsx'],
+  extends: ["standard-with-typescript", "standard-jsx", "prettier"],
   parserOptions: {
-    project: './tsconfig.json'
+    project: "./tsconfig.json",
   },
   rules: {
-    '@typescript-eslint/semi': ['error', 'always'],
-    '@typescript-eslint/no-extra-semi': 'error',
-    '@typescript-eslint/explicit-function-return-type': 0,
-    '@typescript-eslint/strict-boolean-expressions': 0,
-    '@typescript-eslint/no-floating-promises': 0,
-    '@typescript-eslint/prefer-nullish-coalescing': 0,
-    'no-useless-return': 0,
-    '@typescript-eslint/restrict-template-expressions': 0
-  }
-}
+    "@typescript-eslint/semi": ["error", "always"],
+    "@typescript-eslint/no-extra-semi": "error",
+    "@typescript-eslint/explicit-function-return-type": 0,
+    "@typescript-eslint/strict-boolean-expressions": 0,
+    "@typescript-eslint/no-floating-promises": 0,
+    "@typescript-eslint/prefer-nullish-coalescing": 0,
+    "no-useless-return": 0,
+    "@typescript-eslint/restrict-template-expressions": 0,
+  },
+};
--- src/js/components/TheQuran/Stream.tsx
+++ src/js/components/TheQuran/Stream.tsx
@@ -1,30 +1,35 @@
-import { Surah, Ayat, Ayah } from 'lib/Quran';
-import React, { useEffect } from 'react';
-import classNames from 'classnames';
+import { Surah, Ayat, Ayah } from "lib/Quran";
+import React, { useEffect } from "react";
+import classNames from "classnames";
 
 interface StreamProps {
-  surah: Surah
-  stream: Ayat
+  surah: Surah;
+  stream: Ayat;
 }
 
-export function Stream ({ surah, stream }: StreamProps) {
+export function Stream({ surah, stream }: StreamProps) {
   const endOfStream = stream.length === surah.ayat.length;
   const ayat = stream.map((ayah: Ayah) => {
     return (
-      <li key={ayah.id} className='ayah fade'>
-        <span>Surah {surah.id}, Ayah {ayah.id}</span>
+      <li key={ayah.id} className="ayah fade">
+        <span>
+          Surah {surah.id}, Ayah {ayah.id}
+        </span>
         <p>{ayah.text}</p>
       </li>
     );
   });
 
   useEffect(() => {
-    const el: HTMLElement = document.querySelector('ul.stream');
-    el.scroll({ top: el.offsetHeight * stream.length, behavior: 'smooth' });
+    const el: HTMLElement = document.querySelector("ul.stream");
+    el.scroll({
+      top: el.offsetHeight * stream.length,
+      behavior: "smooth",
+    });
   }, [stream]);
 
   return (
-    <ul className={classNames('stream', { 'scroll-y': endOfStream })}>
+    <ul className={classNames("stream", { "scroll-y": endOfStream })}>
       {ayat}
     </ul>
   );
