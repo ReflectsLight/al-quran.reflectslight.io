f5c887f4cc72049e88e0c58c78a183712beac0d6
Gyen Abubakar <indehyde@gmail.com>
2022-11-11 16:11:34 +0000

    eslint & prettier setup

--- src/js/pages/TheSurahPage.tsx
+++ src/js/pages/TheSurahPage.tsx
@@ -1,61 +1,63 @@
-import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom/client';
-import useSurah from 'hooks/useSurah';
-import { Timer } from 'components/TheQuran/Timer';
-import { Stream } from 'components/TheQuran/Stream';
-import { AboutSurah } from 'components/TheQuran/AboutSurah';
-import { ThemeSelect } from 'components/TheQuran/ThemeSelect';
-import classNames from 'classnames';
-import { get as getCookie } from 'es-cookie';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import useSurah from "hooks/useSurah";
+import { Timer } from "components/TheQuran/Timer";
+import { Stream } from "components/TheQuran/Stream";
+import { AboutSurah } from "components/TheQuran/AboutSurah";
+import { ThemeSelect } from "components/TheQuran/ThemeSelect";
+import classNames from "classnames";
+import { get as getCookie } from "es-cookie";
 
 interface PageProps {
-  locale: string
-  surahId: number
+  locale: string;
+  surahId: number;
 }
 
-function TheSurahPage ({ locale, surahId }: PageProps) {
+function TheSurahPage({ locale, surahId }: PageProps) {
   const { surahIsLoaded, surah } = useSurah(locale, surahId);
   const [stream, setStream] = useState([]);
-  const [theme, setTheme] = useState(getCookie('theme') || 'moon');
+  const [theme, setTheme] = useState(getCookie("theme") || "moon");
   const streamIsLoaded = !(stream.length === 0);
 
   useEffect(() => {
     if (surahIsLoaded) {
       document.title = [
-        'Al-Quran:',
+        "Al-Quran:",
         surah.transliteratedName,
-        `(${surah.translatedName})`
-      ].join(' ');
+        `(${surah.translatedName})`,
+      ].join(" ");
       setStream([surah.ayat[stream.length]]);
     }
   }, [surahIsLoaded]);
 
   return (
-    <div className={classNames(theme, locale, 'theme')}>
-      <div className='flex-image'>
-        <div className='image' />
+    <div className={classNames(theme, "theme")}>
+      <div className="flex-image">
+        <div className="image" />
       </div>
-      {streamIsLoaded &&
-        <div className='flex-row'>
+      {streamIsLoaded && (
+        <div className="flex-row">
           <span />
           <ThemeSelect theme={theme} setTheme={setTheme} />
           <span />
-        </div>}
+        </div>
+      )}
       {streamIsLoaded && <AboutSurah surah={surah} />}
       {streamIsLoaded && <Stream surah={surah} stream={stream} />}
-      {streamIsLoaded && stream.length < surah.numberOfAyah &&
+      {streamIsLoaded && stream.length < surah.numberOfAyah && (
         <Timer
           surah={surah}
           ayah={surah.ayat[stream.length - 1]}
           setStream={setStream}
           stream={stream}
-        />}
+        />
+      )}
     </div>
   );
 }
 
-const el = document.querySelector('.surah');
-const locale = el.getAttribute('data-locale');
-const surahId = parseInt(el.getAttribute('data-surah-id'));
+const el = document.querySelector(".surah");
+const locale = el.getAttribute("data-locale");
+const surahId = parseInt(el.getAttribute("data-surah-id"));
 const root = ReactDOM.createRoot(el);
 root.render(<TheSurahPage locale={locale} surahId={surahId} />);
