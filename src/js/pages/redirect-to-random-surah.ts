eb724a14b4b46f8402a846aea68ac5e6144eeaf5
Gyen Abubakar <indehyde@gmail.com>
2022-11-11 18:15:31 +0000

    redirect to Al-Kahf if today's Friday

--- src/js/pages/redirect-to-random-surah.ts
+++ src/js/pages/redirect-to-random-surah.ts
@@ -1,7 +1,11 @@
-import { getLocale } from 'lib/locales';
+import { getLocale } from "lib/locales";
 
 (function () {
-  const surahId: number = Math.ceil(Math.random() * 114);
+  let surahId: number = Math.ceil(Math.random() * 114);
   const locale: string = getLocale();
+
+  const day = new Date().getDay();
+  if (day === 5) surahId = 18;
+
   location.replace(`/${locale}/${surahId}`);
 })();
