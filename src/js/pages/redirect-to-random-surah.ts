8b01a5f5754ca562459bb4d08e10d158821c15f7
0x1eef <0x1eef@protonmail.com>
2022-11-19 19:23:48 -0300

    redirect from index.html with one redirect rather than two.
    Fix #41

--- src/js/pages/redirect-to-random-surah.ts
+++ src/js/pages/redirect-to-random-surah.ts
@@ -3,9 +3,10 @@ import { getLocale } from "lib/locales";
 (function () {
   let surahId: number = Math.ceil(Math.random() * 114);
   const locale: string = getLocale();
-
+  const el: HTMLElement = document.querySelector(".surah-id-to-slug");
+  const slugs = JSON.parse(el.innerText);
   const day = new Date().getDay();
-  if (day === 5) surahId = 18;
 
-  location.replace(`/${locale}/${surahId}`);
+  if (day === 5) surahId = 18;
+  location.replace(`/${locale}/${slugs[surahId]}`);
 })();
