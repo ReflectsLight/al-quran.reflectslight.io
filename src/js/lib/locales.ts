25e5703e1a6a6ad167c1435210388fa6863d7feb
0x1eef <0x1eef@protonmail.com>
2023-01-08 12:29:32 -0300

    Add random redirect from the paths /en, and /ar
    When the theme logo is clicked, the user will be redirected
    to a random surah and the current active language will remain
    the same.

--- src/js/lib/locales.ts
+++
@@ -1,6 +0,0 @@
-export const locales = ['ar', 'en'];
-
-export function getLocale (): string {
-  const userAgentLocales = navigator.languages.map((lang) => lang.substr(0, 2));
-  return userAgentLocales.find((locale) => locales.includes(locale)) || 'en';
-}
