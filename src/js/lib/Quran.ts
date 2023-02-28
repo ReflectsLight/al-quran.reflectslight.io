f62b3df8c515e3ce1ad3efa37f83376124ac7bc3
0x1eef <0x1eef@protonmail.com>
2023-02-28 03:29:57 -0300

    linter: fix

--- src/js/lib/Quran.ts
+++ src/js/lib/Quran.ts
@@ -1,8 +1,8 @@
-import * as JSON from "lib/Quran/JSON";
-import { Surah } from "lib/Quran/Surah";
+import * as JSON from 'lib/Quran/JSON';
+import { Surah } from 'lib/Quran/Surah';
 
 type Locale = 'ar' | 'en';
-type Ayah = {id: number, text: string, readTimeMs: number };
+interface Ayah {id: number, text: string, readTimeMs: number }
 type Ayat = Ayah[];
 
 export { Surah, Ayah, Ayat, Locale, JSON };
