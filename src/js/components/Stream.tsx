914c918eaa60e50fe75ae5e99a1826d38c877efa
0x1eef <0x1eef@protonmail.com>
2023-03-10 16:36:52 -0300

    Add a simpler, more organized i18n.ts implementation

--- src/js/components/Stream.tsx
+++ src/js/components/Stream.tsx
@@ -1,6 +1,6 @@
 import * as Quran from 'lib/Quran';
 import React, { useEffect } from 'react';
-import { numbers, strings } from 'lib/i18n';
+import { TFunction } from 'lib/i18n';
 import { Slice } from 'lib/Quran/Slice';
 import classNames from 'classnames';
 
@@ -11,21 +11,20 @@ interface Props {
   slice: Slice
   endOfStream: boolean
   isPaused: boolean
+  t: TFunction
 }
 
-export function Stream({ surah, stream, locale, slice, endOfStream, isPaused }: Props) {
-  const n = numbers(locale);
-  const s = strings(locale);
+export function Stream({ surah, stream, locale, slice, endOfStream, isPaused, t }: Props) {
   const className = classNames('body', 'stream', { 'scroll-y': endOfStream || isPaused });
   const ayat = stream.map((ayah: Quran.Ayah) => {
     return (
       <li key={ayah.id} className="ayah fade">
         <span className="surah-id ayah-id">
-          {s('surah')}{' '}
-          {n(surah.id)}
-          {s('comma')}{' '}
-          {s('ayah')}{' '}
-          {n(ayah.id)}
+          {t(locale, 'surah')}{' '}
+          {surah.id.toLocaleString(locale)}
+          {t(locale, 'comma')}{' '}
+          {t(locale, 'ayah')}{' '}
+          {ayah.id.toLocaleString(locale)}
         </span>
         <p>{ayah.text}</p>
       </li>
