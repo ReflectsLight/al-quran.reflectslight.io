f691875012bfad9dfebda5e974002ffa6d4d6012
0x1eef <0x1eef@protonmail.com>
2023-03-11 14:07:46 -0300

    Replace "File.read" with inline_json

--- src/js/pages/surah/random.ts
+++ src/js/pages/surah/random.ts
@@ -3,7 +3,7 @@ import { Locale } from 'lib/Locale';
 (function () {
   const surahId: number = Math.ceil(Math.random() * 114);
   const locale = Locale(window);
-  const el: HTMLElement = document.querySelector('.surah-id-to-slug')!;
+  const el: HTMLElement = document.querySelector('.json.slugs')!;
   const slugs = JSON.parse(el.innerText);
   location.replace(`/${locale.fromPath()}/${slugs[surahId]}`);
 })();
