145d5a4399cf820878c84c94202d5d4a5927257f
0x1eef <0x1eef@protonmail.com>
2023-03-11 09:31:37 -0300

    Run rubocop -A

--- lib/helper.rb
+++ lib/helper.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 module Helper
   def inline_json(path)
-    className = File.basename(path, File.extname(path))
-    "<script class='json #{className}' type='application/json'>" \
+    class_name = File.basename(path, File.extname(path))
+    "<script class='json #{class_name}' type='application/json'>" \
     "#{items[path].compiled_content}" \
     "</script>"
   end
