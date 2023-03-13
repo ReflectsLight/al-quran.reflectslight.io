ae2f4f23c5f3261a5bb5a11db038c23b6903cef8
0x1eef <0x1eef@protonmail.com>
2023-03-13 20:20:35 -0300

    Synchronize ?ayah=x parameter on language change
    When given - for example, `?ayah=5` and then the language is
    changed at ayah 7, the "ayah" parameter should be updated to 7.
    
    Fix #85

--- src/js/lib/Quran/Slice.ts
+++ src/js/lib/Quran/Slice.ts
@@ -1,35 +1,34 @@
 export interface Slice {
   begin: number
-  end: number
-  length: number
+  end: number | null
   coversOneAyah: boolean
   coversOneSurah: boolean
   coversSubsetOfSurah: boolean
+  subsetLength: number
   toParam: () => string | null
 }
 
-export function Slice(begin: number, end: number): Slice {
+export function Slice(begin: number, end: number | null): Slice {
   const self: Slice = Object.create(null);
 
   self.begin = begin;
   self.end = end;
-  self.coversOneAyah = begin === end;
+  self.coversOneAyah = end === null;
   self.coversOneSurah = begin === 1 && end === 286;
-  self.coversSubsetOfSurah = begin >= 1 && end < 286;
-  self.length = end - (begin - 1);
-
-  self.toParam = () => {
-    if (self.coversOneAyah) {
-      return `${self.begin}`;
-    } else if(self.coversSubsetOfSurah) {
-      return `${self.begin}..${self.end}`;
-    } else {
-      return null;
-    }
-  };
+  self.coversSubsetOfSurah = end !== null && begin >= 1 && end < 286;
+  self.subsetLength = getSubsetLength(self);
 
   return self;
 }
+
+const getSubsetLength = (slice: Slice) => {
+  const { begin, end } = slice;
+  if (end) {
+    return end - (begin - 1);
+  } else {
+    return 0;
+  }
+};
 
 const digitsRange = /^(\d{1,3})\.\.(\d{1,3})$/;
 const digits = /^\d{1,3}$/;
@@ -42,7 +41,7 @@ Slice.fromParam = function(param: string | null): Slice {
     const [, begin, end] = match;
     return Slice(parseInt(begin), parseInt(end));
   } else if (digits.test(param)) {
-    return Slice(parseInt(param), parseInt(param));
+    return Slice(parseInt(param), null);
   } else {
     return Slice(1, 286);
   }
