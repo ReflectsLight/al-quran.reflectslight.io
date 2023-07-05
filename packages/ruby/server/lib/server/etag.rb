d5704b081355ce57b57ad4e1641595ee68cbea7a
0x1eef <0x1eef@protonmail.com>
2023-07-05 15:28:00 -0300

    Add If-None-Match support to Server::ETag

--- packages/ruby/server/lib/server/etag.rb
+++ packages/ruby/server/lib/server/etag.rb
@@ -1,15 +1,19 @@
 # frozen_string_literal: true
 
 class Server::ETag < Rack::ETag
+  ETAGS = {}
+
   def initialize(app)
     @app = app
   end
 
   def call(env)
-    status, headers, body = super(env)
-    if env["HTTP_IF_NONE_MATCH"] == headers["etag"]
+    headers = ETAGS[env["REQUEST_PATH"]] || {}
+    if headers["etag"] && headers["etag"] == env["HTTP_IF_NONE_MATCH"]
       [304, headers, [""]]
     else
+      status, headers, body = super(env)
+      ETAGS[env["REQUEST_PATH"]] = headers
       [status, headers, body]
     end
   end
