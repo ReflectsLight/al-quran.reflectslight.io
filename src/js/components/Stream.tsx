dd833a384d26f496979db9d19944b0a49e007071
0x1eef <0x1eef@protonmail.com>
2023-07-13 12:18:04 -0300

    Additional changes for iOS
    Still not flawless. More work required.

--- src/js/components/Stream.tsx
+++ src/js/components/Stream.tsx
@@ -1,31 +1,24 @@
 import * as Quran from 'lib/Quran';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import { formatNumber, TFunction } from 'lib/i18n';
 import { Slice } from 'lib/Quran/Slice';
 import classNames from 'classnames';
 
 interface Props {
-  reciter: Quran.Reciter
   surah: Quran.Surah
   stream: Quran.Ayat
   locale: Quran.Locale
   slice: Slice
   endOfStream: boolean
   isPaused: boolean
-  soundOn: boolean
-  setSoundOn: (v: boolean) => void
   t: TFunction
 }
 
-export function Stream({ reciter, surah, stream, locale, slice, endOfStream, isPaused, setSoundOn, soundOn, t }: Props) {
+export function Stream({ surah, stream, locale, slice, endOfStream, isPaused, t }: Props) {
   const className = classNames('body', 'stream');
   const style: React.CSSProperties = endOfStream || isPaused ?
                                      { 'overflowY': 'auto' } :
                                      { 'overflowY': 'hidden' };
-  const audioRef = useRef<HTMLAudioElement>(null);
-  const { url: baseUrl } = reciter;
-  const ayah = stream[stream.length-1];
-  const src = `${baseUrl}/${surah.id}/${ayah.id}.mp3`;
   const ayat = stream.map((ayah: Quran.Ayah) => {
     return (
       <li key={ayah.id} className="ayah fade">
@@ -51,31 +44,9 @@ export function Stream({ reciter, surah, stream, locale, slice, endOfStream, isP
     }
   }, [stream]);
 
-  useEffect(() => {
-    const audio = audioRef.current;
-    if (!audio) {
-      return;
-    } else if (isPaused || !soundOn) {
-      audio.pause();
-    } else if (soundOn) {
-      audio.play()
-           .catch(() => setSoundOn(false));
-    }
-  }, [stream, isPaused, soundOn]);
-
-  useEffect(() => {
-    const audio = audioRef.current;
-    if (audio) {
-      audio.addEventListener('ended', () => {
-        audio.setAttribute('src', src);
-      });
-    }
-  }, [audioRef.current]);
-
   return (
     <ul lang={locale} className={className} style={style}>
       {ayat}
-      <audio src={src} ref={audioRef} />
     </ul>
   );
 }
