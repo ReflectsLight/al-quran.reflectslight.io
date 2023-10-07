88630bae1b2ae6a7d22d45a0adf5d66670953123
0x1eef <0x1eef@protonmail.com>
2023-10-07 03:26:43 -0300

    'erb' is relative to the html directory

--- nanoc/lib/mixin/erb.rb
+++ nanoc/lib/mixin/erb.rb
@@ -1,6 +1,6 @@
 module Mixin::ERB
-  def erb(partial, local_assigns = {})
-    erb = File.binread File.join(Dir.getwd, "src", "html", "partials", partial)
+  def erb(path, local_assigns = {})
+    erb = File.binread File.join(Dir.getwd, "src", "html", path)
     scope = binding
     local_assigns.each { scope.local_variable_set(_1, _2) }
     ::ERB.new(erb).result(scope)
