b418b47685df908be7689298a107ed31374128fc
0x1eef <0x1eef@protonmail.com>
2023-10-07 17:44:58 -0300

    Add 'memoize'

--- nanoc/lib/mixin.rb
+++ nanoc/lib/mixin.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 module Mixin
+  require "memoize"
+  extend Memoize
+
   require_relative "mixin/t"
   require_relative "mixin/inline"
   require_relative "mixin/erb"
@@ -10,6 +13,7 @@ module Mixin
     nanoc = Ryo.from YAML.load_file(File.join(Dir.getwd, "nanoc.yaml"))
     nanoc.output_dir
   end
+  memoize :build_dir
 
   include T
   include Inline
--- nanoc/lib/mixin/t.rb
+++ nanoc/lib/mixin/t.rb
@@ -1,14 +1,15 @@
 module Mixin::T
+  extend Memoize
+
   def t(locale, key, local_assigns = {})
     str = [locale, *key.split(".")].inject(i18n) { _1[_2] }
     format(str, local_assigns)
   end
 
   def i18n
-    @i18n ||= Ryo.from(
-      JSON.parse(
-        File.read(File.join(Dir.getwd, "src", "i18n.json"))
-      )
-    )
+    path = File.join(Dir.getwd, "src", "i18n.json")
+    json = File.binread(path)
+    Ryo.from JSON.parse(json)
   end
+  memoize :i18n
 end
