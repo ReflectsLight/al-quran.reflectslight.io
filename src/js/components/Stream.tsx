01d7e4f6fb8f0443ec509ef5a53cfe5a56d28a42
0x1eef <0x1eef@protonmail.com>
2023-12-04 00:14:35 -0300

    Add "OFFSET" to Stream.tsx

--- src/js/components/Stream.tsx
+++ src/js/components/Stream.tsx
@@ -14,6 +14,14 @@ interface Props {
   t: TFunction;
 }
 
+/*
+  Provides a number that increments the value of the 'top'
+  parameter given to scrollBy() during auto-scroll. This
+  variable came from the scroll height provided by iOS being
+  less than expected.
+*/
+const OFFSET = 100;
+
 export function Stream({
   recitation,
   surah,
