043523e48a4e714ae55cf04a42865980be24fee4
0x1eef <0x1eef@protonmail.com>
2024-03-12 12:34:18 -0300

    Replace 'es-cookie' with 'document.cookie'

--- src/js/hooks/useTheme.ts
+++ src/js/hooks/useTheme.ts
@@ -1,23 +1,23 @@
 import { useState } from "react";
-import { get as getCookie, set as setCookie } from "es-cookie";
 
-type Theme = "blue" | "green";
+export type Theme = "blue" | "green";
+type Result = [Theme, (t: Theme) => void];
 const THEMES: Theme[] = ["blue", "green"];
 const DEFAULT_THEME = "blue";
 
-export function useTheme(): [Theme, (t: string) => void] {
-  const cookie = getCookie("theme");
-  const [theme, setTheme] = useState<Theme>(
-    () => THEMES.find(t => t === cookie) || DEFAULT_THEME,
+export function useTheme(): Result {
+  const cookies = Object.fromEntries(
+    document.cookie.split(";").map(e => e.split("=")),
   );
-
-  function _setTheme(newTheme: string) {
-    const matchedTheme = THEMES.find((theme: Theme) => newTheme === theme);
-    if (matchedTheme) {
-      setCookie("theme", matchedTheme, { domain: location.host, expires: 365 });
-      setTheme(matchedTheme);
+  const [theme, setTheme] = useState<Theme>(
+    THEMES.find(t => t === cookies.theme) || DEFAULT_THEME,
+  );
+  const set = (t: Theme) => {
+    if (THEMES.includes(t)) {
+      const maxAge = 3600 * 24 * 90;
+      document.cookie = `theme=${t}; path=/; max-age=${maxAge}`;
+      setTheme(t);
     }
-  }
-
-  return [theme, _setTheme];
+  };
+  return [theme, set];
 }
