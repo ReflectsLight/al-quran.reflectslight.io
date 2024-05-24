7d8867e9915e3598997450e74677d7da1919655d
0x1eef <0x1eef@protonmail.com>
2024-05-24 15:56:52 -0300

    Fix bug in redirect.ts

--- rake/tasks/ci.rake
+++ rake/tasks/ci.rake
@@ -2,8 +2,17 @@
 
 desc "Run CI tasks"
 task :ci do
+  ##
+  # format
   sh "bundle exec rubocop"
   sh "npm exec eslint -- src/js/"
   sh "npm exec prettier -- --check src/js/"
+
+  ##
+  # tsc
+  sh "npm run tsc"
+
+  ##
+  # build
   Bundler.with_original_env { sh "buildenv=production rake nanoc:clean nanoc:build" }
 end
--- src/js/main/redirect.ts
+++ src/js/main/redirect.ts
@@ -1,9 +1,10 @@
-import { Quran, TLocale } from "Quran";
+import { Quran } from "Quran";
 (function () {
   const defaultl = "en";
+  const locales = Quran.locales.map(l => l.name);
   const locale =
     navigator.languages
       .map(s => s.slice(0, 2).toLowerCase())
-      .find(s => Quran.locales.includes(s as TLocale)) || defaultl;
+      .find(s => locales.includes(s)) || defaultl;
   location.replace(`/${locale}/`);
 })();
