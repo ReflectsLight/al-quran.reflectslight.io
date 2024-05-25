6db1792828d96b442af6c58ce3e6dc00743c7ea5
0x1eef <0x1eef@protonmail.com>
2024-05-24 22:02:03 -0300

    Update src/js/loaders/

--- src/js/loaders/SurahIndexLoader.ts
+++ src/js/loaders/SurahIndexLoader.ts
@@ -1,10 +1,10 @@
 import postman, { item } from "postman";
 
 (function () {
-  const parent: HTMLElement = document.querySelector(".postman.loader")!;
-  const progressBar: HTMLProgressElement = parent.querySelector("progress")!;
-  const progressNumber: HTMLSpanElement = parent.querySelector(".percentage")!;
-  const inlineStyle: HTMLStyleElement = document.querySelector(".css.postman")!;
+  const container: HTMLElement = document.querySelector(".postman.loader")!;
+  const progressBar: HTMLProgressElement = container.querySelector("progress")!;
+  const progressNumber: HTMLSpanElement = container.querySelector(".percentage")!;
+  const stylesheet: HTMLStyleElement = document.querySelector(".css.postman")!;
 
   postman(
     item.script("/js/main/surah-index.js"),
@@ -18,8 +18,8 @@ import postman, { item } from "postman";
   )
     .fetch()
     .then(pkg => {
-      inlineStyle.remove();
-      parent.remove();
+      stylesheet.remove();
+      container.remove();
       pkg.fonts.forEach(f => document.fonts.add(f));
       pkg.css.forEach(s => document.head.appendChild(s));
       pkg.scripts.forEach(s => document.body.removeChild(document.body.appendChild(s)));
