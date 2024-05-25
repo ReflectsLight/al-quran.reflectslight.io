f687a99a7b078bde38125bbe2765b73c76c5e08a
0x1eef <0x1eef@protonmail.com>
2024-05-25 02:42:56 -0300

    Try avoid random build failure with postman.scss

--- nanoc/lib/mixin/inline.rb
+++ nanoc/lib/mixin/inline.rb
@@ -10,7 +10,7 @@ module Mixin::Inline
   def inline_css(path)
     class_name = File.basename(path, File.extname(path))
     "<style class='css #{class_name}'>" \
-    "#{File.binread(File.join(build_dir, path))}" \
+    "#{items[path.sub('.css', '.scss')].compiled_content}" \
     "</style>"
   end
 end
