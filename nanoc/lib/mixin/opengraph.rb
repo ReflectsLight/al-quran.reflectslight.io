76bb19e3acf23006578ce23a38a75ef59e371f64
0x1eef <0x1eef@protonmail.com>
2024-05-25 23:18:41 -0300

    Fix opengraph.rb

--- nanoc/lib/mixin/opengraph.rb
+++ nanoc/lib/mixin/opengraph.rb
@@ -11,18 +11,18 @@ module Mixin::OpenGraph
       surah = context.surah
       {title: t(context.locale, "TheNobleQuran"),
        description: surah.name,
-       url: "https://<%= hostname %>/#{context.locale}/#{surah.slug}/",
-       image: "https://<%= hostname %>/images/og/#{surah.id}.png"}
+       url: "https://#{hostname}/#{context.locale}/#{surah.slug}/",
+       image: "https://#{hostname}/images/og/#{surah.id}.png"}
     when "redirect.html.erb", "surah-index.html.erb"
       {title: t(context.locale, "TheNobleQuran"),
        description: t(context.locale, "meta.index.description"),
-       url: "https://<%= hostname %>/#{context.locale}/",
-       image: "https://<%= hostname %>/images/og/0.png"}
+       url: "https://#{hostname}/#{context.locale}/",
+       image: "https://#{hostname}/images/og/0.png"}
     when "random.html.erb"
       {title: t(context.locale, "TheNobleQuran"),
        description: t(context.locale, "meta.random.description"),
-       url: "https://<%= hostname %>/#{context.locale}/random/",
-       image: "https://<%= hostname %>/images/og/0.png"}
+       url: "https://#{hostname}/#{context.locale}/random/",
+       image: "https://#{hostname}/images/og/0.png"}
     end
   end
 end
