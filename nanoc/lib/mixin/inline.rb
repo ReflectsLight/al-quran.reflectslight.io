efbd0299f91e99c308c529d150ab251fab838405
0x1eef <0x1eef@protonmail.com>
2024-05-26 02:56:50 -0300

    Replace single quotes with double quotes

--- nanoc/lib/mixin/inline.rb
+++ nanoc/lib/mixin/inline.rb
@@ -2,14 +2,14 @@
 
 module Mixin::Inline
   def inline_json(path, class_name: File.basename(path, File.extname(path)))
-    "<script class='json #{class_name}' type='application/json'>" \
+    %|<script class="json #{class_name}" type="application/json">| \
     "#{items[path].compiled_content}" \
     "</script>"
   end
 
   def inline_css(path)
     class_name = File.basename(path, File.extname(path))
-    "<style class='css #{class_name}'>" \
+    %|<style class="css #{class_name}">| \
     "#{items[path.sub(".css", ".scss")].compiled_content}" \
     "</style>"
   end
