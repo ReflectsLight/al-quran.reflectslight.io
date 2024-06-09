cf8421b34dd61ec2f90229c701a35c50fb853938
0x1eef <0x1eef@protonmail.com>
2024-06-09 02:20:32 -0300

    Bump header font-size for rtl languages

--- src/js/components/Head.tsx
+++ src/js/components/Head.tsx
@@ -3,6 +3,7 @@ import type { ReactNode } from "react";
 import { LanguageSelect, ThemeSelect } from "~/components/Select";
 import type { TLocale } from "Quran";
 import { Theme } from "~/hooks/useTheme";
+import classNames from "classnames";
 
 type Props = {
   locale: TLocale;
@@ -13,7 +14,12 @@ type Props = {
 
 export function Head({ locale, theme, setTheme, children }: Props) {
   return (
-    <header className="flex flex-col h-12 mt-4 mb-4">
+    <header
+      className={classNames("flex flex-col h-12 mt-4", {
+        "mb-4": locale.direction === "ltr",
+        "mb-6": locale.direction === "rtl",
+      })}
+    >
       <h1 className="flex justify-center p-0 m-0 mb-4">
         <a className="no-underline" href={`/${locale.name}/`}>
           {children}
