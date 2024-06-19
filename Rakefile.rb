8f3a245f9a7a51507e7f5a9ab5515376b1cce185
0x1eef <0x1eef@protonmail.com>
2024-06-19 17:56:38 -0300

    Add rake/tasks/server.rake

--- Rakefile.rb
+++ Rakefile.rb
@@ -4,8 +4,6 @@ require "bundler/setup"
 require "ryo"
 require "yaml"
 
-##
-# Rake tasks
 begin
   load "rake/tasks/deploy.rake"
 rescue LoadError
@@ -15,20 +13,5 @@ load "rake/tasks/format.rake"
 load "rake/tasks/nanoc.rake"
 load "rake/tasks/t.rake"
 load "rake/tasks/ci.rake"
-
-desc "Start HTTP server"
-task :server, [:protocol] do |_t, args|
-  require "server"
-  nanoc = Ryo.from(YAML.load_file("./nanoc.yaml"))
-  h = args.to_h
-  o = if h[:protocol] == "unix"
-    {unix: nanoc.server.unix.path}
-  else
-    {host: nanoc.server.tcp.host, port: nanoc.server.tcp.port}
-  end
-  s = Server.dir(nanoc.output_dir, o)
-  s.start(block: true)
-rescue Interrupt
-  s.stop
-end
+load "rake/tasks/server.rake"
 task default: "nanoc:build"
