45723a3ddcd742d0d91974108d5dcc034bdd1e04
0x1eef <0x1eef@protonmail.com>
2024-06-22 19:21:16 -0300

    Fix example in API docs

--- nanoc/lib/nanoc/extension/anonymous_filter.rb
+++ nanoc/lib/nanoc/extension/anonymous_filter.rb
@@ -11,7 +11,7 @@ module Nanoc::Extension
     # @example
     #   compile "/sitemap.xml.erb" do
     #     filter(:erb)
-    #     filter Proc.new { _1.each_line.reject { |s| s.strip.empty? } }
+    #     filter Proc.new { _1.each_line.reject { |s| s.strip.empty? }.join }
     #     write("/sitemap.xml")
     #   end
     # @param [Proc, Symbol] fn
