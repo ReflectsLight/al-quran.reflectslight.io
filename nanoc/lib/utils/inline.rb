b6a7ae3ee6cbb1b1540fe2d4896d34110ebee296
0x1eef <0x1eef@protonmail.com>
2024-06-22 01:40:56 -0300

    Remove unused method (inline_json)

--- nanoc/lib/utils/inline.rb
+++ nanoc/lib/utils/inline.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 
 module Utils::Inline
-  def inline_json(path, class_name: File.basename(path, File.extname(path)))
-    %|<script class="json #{class_name}" type="application/json">| \
-    "#{items[path].compiled_content}" \
-    "</script>"
-  end
-
   def inline_css(path)
     class_name = File.basename(path, File.extname(path))
     %|<style class="css #{class_name}">| \
