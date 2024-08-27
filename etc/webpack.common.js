cda1974e4fd8a3d1d70c01463fb9db55e2204a0d
0x1eef <0x1eef@protonmail.com>
2024-08-26 22:57:34 -0300

    React -> preact

--- etc/webpack.common.js
+++ etc/webpack.common.js
@@ -8,6 +8,11 @@ module.exports = {
       "@css": path.resolve(__dirname, "..", "src", "css"),
       "@json": path.resolve(__dirname, "..", "src", "json"),
       "~": path.resolve(__dirname, "..", "src", "js"),
+      "react": "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      // Must be below test-utils
+      "react-dom": "preact/compat",
+      "react/jsx-runtime": "preact/jsx-runtime"
     },
     extensions: [".js", ".ts", ".tsx", ".json", ".css", ".scss"],
   },
