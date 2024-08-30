0d82269bc827a1478af351840684d6a4f208ae75
0x1eef <0x1eef@protonmail.com>
2024-08-30 07:49:33 -0300

    Re-add corejs

--- src/js/main/vendor.ts
+++ src/js/main/vendor.ts
@@ -2,6 +2,7 @@ import { render } from "preact";
 import { useState, useEffect, useMemo, useRef } from "preact/hooks";
 import * as React from "preact/compat";
 import classNames from "classnames";
+import "core-js";
 
 const exports = {
   React,
