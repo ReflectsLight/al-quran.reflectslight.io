a981d580979d337ad660b86a8fa02023c2bdb4bd
0x1eef <0x1eef@protonmail.com>
2024-11-27 11:31:26 -0300

    Revisit SurahIndexLoader, SurahStreamLoader

--- src/js/loaders/SurahIndexLoader.ts
+++ src/js/loaders/SurahIndexLoader.ts
@@ -1,39 +1,45 @@
 import postman, { item } from "postman";
 import { formatNumber } from "~/lib/t";
 
-(async function () {
+(function () {
   const doc = document.documentElement;
-  const main: HTMLElement = doc.querySelector(".postman.main")!;
-  const css: HTMLStyleElement = doc.querySelector(".postman.css")!;
-  const rev = doc
-    .querySelector("meta[name='revision']")!
-    .getAttribute("content")!;
+  const rev = doc.querySelector("meta[name='revision']")!.getAttribute("content")!;
 
   function getFonts() {
     return [
       item.font("Cairo Regular", "url(/fonts/cairo-regular.ttf)"),
       item.font("Kanit Regular", "url(/fonts/kanit-regular.ttf)"),
-      doc.dir === "rtl" ? item.font("Cairo Bold", "url(/fonts/cairo-bold.ttf)") : null
+      doc.dir === "rtl" ? item.font("Cairo Bold", "url(/fonts/cairo-bold.ttf)") : null,
     ].filter((i) => !!i);
   }
 
-  const parcel = await postman(
-    item.script(`/js/main/vendor.js?v=${rev}`, { id: "0" }),
-    item.script(`/js/main/surah-index.js?v=${rev}`, { id: "1" }),
-    ...getFonts(),
-    item.progress((percent: number) => {
-      const bar: HTMLProgressElement = main.querySelector("progress")!;
-      const num: HTMLSpanElement = main.querySelector(".percentage")!;
-      bar.value = percent;
-      num.innerText = formatNumber(doc.lang, Number(percent.toFixed(0)));
-    }),
-  ).deliver();
-  [main, css].forEach((el) => el.remove());
-  parcel.fonts.forEach((f) => document.fonts.add(f));
-  parcel.css.forEach((s: HTMLElement) => document.head.appendChild(s));
-  parcel.scripts
-    .sort((a, b) => Number(a.id) - Number(b.id))
-    .forEach((s) => {
-      document.body.removeChild(document.body.appendChild(s));
-    });
+  function deliver() {
+    return postman(
+      ...getFonts(),
+      item.script(`/js/main/vendor.js?v=${rev}`, { id: "0" }),
+      item.script(`/js/main/surah-index.js?v=${rev}`, { id: "1" }),
+      item.progress((percent: number) => {
+        const main: HTMLElement = doc.querySelector(".postman.main")!;
+        const bar: HTMLProgressElement = main.querySelector("progress")!;
+        const num: HTMLSpanElement = main.querySelector(".percentage")!;
+        bar.value = percent;
+        num.innerText = formatNumber(doc.lang, Number(percent.toFixed(0)));
+      }),
+    ).deliver();
+  }
+
+  async function unpack() {
+    const delivery = await deliver();
+    const main: HTMLElement = doc.querySelector(".postman.main")!;
+    const css: HTMLStyleElement = doc.querySelector(".postman.css")!;
+    [main, css].forEach((el) => el.remove());
+    delivery.fonts.forEach((f) => document.fonts.add(f));
+    delivery.css.forEach((s: HTMLElement) => document.head.appendChild(s));
+    delivery.scripts
+      .sort((a, b) => Number(a.id) - Number(b.id))
+      .forEach((s) => {
+        document.body.removeChild(document.body.appendChild(s));
+      });
+  }
+  unpack();
 })();
--- src/js/loaders/SurahStreamLoader.ts
+++ src/js/loaders/SurahStreamLoader.ts
@@ -1,14 +1,10 @@
 import postman, { item } from "postman";
 import { formatNumber } from "~/lib/t";
 
-(async function () {
+(function () {
   const doc = document.documentElement;
-  const main = doc.querySelector(".postman.main")!;
-  const css = doc.querySelector(".postman.css")!;
+  const rev = doc.querySelector("meta[name='revision']")!.getAttribute("content")!;
   const { surahId } = doc.querySelector<HTMLElement>(".app.mount")!.dataset;
-  const rev = doc
-    .querySelector("meta[name='revision']")!
-    .getAttribute("content")!;
 
   function getFonts() {
     return [
@@ -19,27 +15,37 @@ import { formatNumber } from "~/lib/t";
     ].filter((i) => !!i);
   }
 
-  const parcel = await postman(
-    item.script(`/js/main/vendor.js?v=${rev}`, { id: "0" }),
-    item.script(`/js/main/surah-stream.js?v=${rev}`, { id: "1" }),
-    ...getFonts(),
-    item.json(`/json/${doc.lang}/${surahId}/index.json?v=${rev}`, {
-      className: "json surah",
-    }),
-    item.progress((percent: number) => {
-      const bar = main.querySelector("progress")!;
-      const num: HTMLSpanElement = main.querySelector(".percentage")!;
-      bar.value = percent;
-      num.innerText = formatNumber(doc.lang, Number(percent.toFixed(0)));
-    }),
-  ).deliver();
-  [main, css].forEach((el) => el.remove());
-  parcel.fonts.forEach((f) => document.fonts.add(f));
-  parcel.css.forEach((s) => document.head.appendChild(s));
-  parcel.json.forEach((o) => document.body.appendChild(o));
-  parcel.scripts
-    .sort((a, b) => Number(a.id) - Number(b.id))
-    .forEach((s) => {
-      document.body.removeChild(document.body.appendChild(s));
-    });
+  function deliver() {
+    return postman(
+      ...getFonts(),
+      item.script(`/js/main/vendor.js?v=${rev}`, { id: "0" }),
+      item.script(`/js/main/surah-stream.js?v=${rev}`, { id: "1" }),
+      item.json(`/json/${doc.lang}/${surahId}/index.json?v=${rev}`, {
+        className: "json surah",
+      }),
+      item.progress((percent: number) => {
+        const main = doc.querySelector(".postman.main")!;
+        const bar = main.querySelector("progress")!;
+        const num: HTMLSpanElement = main.querySelector(".percentage")!;
+        bar.value = percent;
+        num.innerText = formatNumber(doc.lang, Number(percent.toFixed(0)));
+      }),
+    ).deliver();
+  }
+
+  async function unpack() {
+    const delivery = await deliver();
+    const main = doc.querySelector(".postman.main")!;
+    const css = doc.querySelector(".postman.css")!;
+    [main, css].forEach((el) => el.remove());
+    delivery.fonts.forEach((f) => document.fonts.add(f));
+    delivery.css.forEach((s) => document.head.appendChild(s));
+    delivery.json.forEach((o) => document.body.appendChild(o));
+    delivery.scripts
+      .sort((a, b) => Number(a.id) - Number(b.id))
+      .forEach((s) => {
+        document.body.removeChild(document.body.appendChild(s));
+      });
+  }
+  unpack();
 })();
