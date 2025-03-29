a8b751871c4ad5173bf87db34e6864cd04599fea
0x1eef <0x1eef@protonmail.com>
2025-03-29 15:25:39 -0300

    Revisit loaders

--- src/js/loaders/surah-index-loader.js
+++ src/js/loaders/surah-index-loader.js
@@ -1,12 +1,25 @@
 import { Postman, item } from "@0x1eef/postman"
-;(function () {
-  const { rev, progressBar, progressFile } = getPageState()
-  const postman = Postman(
+
+function getContext() {
+  return {
+    rev: document.querySelector("meta[name='revision']").getAttribute("content"),
+    progressBar: document.querySelector(".postman.main progress"),
+    progressFile: document.querySelector(".postman.main .filename"),
+  }
+}
+
+function getItems(rev) {
+  return [
     item.font("Noto Sans", "/fonts/NotoSans-VariableFont_wdth,wght.ttf"),
     item.font("Noto Sans Arabic", "/fonts/NotoSansArabic-VariableFont_wdth,wght.ttf"),
     item.script(`/js/main/vendor.js?v=${rev}`),
     item.script(`/js/main/surah-index.js?v=${rev}`),
-  )
+  ]
+}
+
+;(function () {
+  const { rev, progressBar, progressFile } = getContext()
+  const postman = Postman(...getItems(rev))
 
   postman.addEventListener("error", (e) => {
     const { controller } = e.detail
@@ -25,23 +38,14 @@ import { Postman, item } from "@0x1eef/postman"
     } else {
       progressBar.value = progress
       progressFile.innerText = href.split("/").pop().replace(/\?.*/, "")
-      if (progress === 100) setTimeout(() => append(parcel), 250)
+      if (progress === 100)
+        setTimeout(() => {
+          document.querySelectorAll(".postman").forEach((el) => el.remove())
+          parcel.fonts.forEach((font) => document.fonts.add(font))
+          parcel.scripts.forEach((script) => document.body.appendChild(script))
+        }, 50)
     }
   })
 
   postman.deliver()
 })()
-
-function getPageState() {
-  return {
-    rev: document.querySelector("meta[name='revision']").getAttribute("content"),
-    progressBar: document.querySelector(".postman.main progress"),
-    progressFile: document.querySelector(".postman.main .percentage"),
-  }
-}
-
-function append(parcel) {
-  document.querySelectorAll(".postman").forEach((el) => el.remove())
-  parcel.fonts.forEach((font) => document.fonts.add(font))
-  parcel.scripts.forEach((script) => document.body.appendChild(script))
-}
--- src/js/loaders/surah-stream-loader.js
+++ src/js/loaders/surah-stream-loader.js
@@ -1,14 +1,39 @@
 import { Postman, item } from "@0x1eef/postman"
-;(function () {
-  const { rev, locale, surahId, progressBar, progressFile } = getPageState()
-  const postman = Postman(
+
+function getContext() {
+  return {
+    locale: document.documentElement.lang,
+    rev: document.querySelector("meta[name='revision']").getAttribute("content"),
+    surahId: document.querySelector(".app.mount").dataset.surahId,
+    progressBar: document.querySelector(".postman.main progress"),
+    progressFile: document.querySelector(".postman.main .filename"),
+  }
+}
+
+function getFonts(locale) {
+  return [
+    item.font("Noto Sans", "/fonts/NotoSans-VariableFont_wdth,wght.ttf"),
+    item.font("Noto Sans Arabic", "/fonts/NotoSansArabic-VariableFont_wdth,wght.ttf"),
+    locale === "ar"
+      ? item.font("Noto Naskh Arabic", "/fonts/NotoNaskhArabic-VariableFont_wght.ttf")
+      : null,
+  ].filter((i) => !!i)
+}
+
+function getItems(locale, surahId, rev) {
+  return [
     ...getFonts(locale),
     item.json(`/json/${locale}/${surahId}/index.json?v=${rev}`, {
       className: "json surah",
     }),
     item.script(`/js/main/vendor.js?v=${rev}`),
     item.script(`/js/main/surah-stream.js?v=${rev}`),
-  )
+  ]
+}
+
+;(function () {
+  const { rev, locale, surahId, progressBar, progressFile } = getContext()
+  const postman = Postman(...getItems(locale, surahId, rev))
 
   postman.addEventListener("error", (e) => {
     const {
@@ -31,36 +56,15 @@ import { Postman, item } from "@0x1eef/postman"
     } else {
       progressBar.value = progress
       progressFile.innerText = href.split("/").pop().replace(/\?.*/, "")
-      if (progress === 100) setTimeout(() => append(parcel), 50)
+      if (progress === 100)
+        setTimeout(() => {
+          document.querySelectorAll(".postman").forEach((el) => el.remove())
+          parcel.json.forEach((json) => document.body.appendChild(json))
+          parcel.fonts.forEach((font) => document.fonts.add(font))
+          parcel.scripts.forEach((script) => document.body.appendChild(script))
+        }, 50)
     }
   })
 
   postman.deliver()
 })()
-
-function getPageState() {
-  return {
-    locale: document.documentElement.lang,
-    rev: document.querySelector("meta[name='revision']").getAttribute("content"),
-    surahId: document.querySelector(".app.mount").dataset.surahId,
-    progressBar: document.querySelector(".postman.main progress"),
-    progressFile: document.querySelector(".postman.main .percentage"),
-  }
-}
-
-function getFonts(locale) {
-  return [
-    item.font("Noto Sans", "/fonts/NotoSans-VariableFont_wdth,wght.ttf"),
-    item.font("Noto Sans Arabic", "/fonts/NotoSansArabic-VariableFont_wdth,wght.ttf"),
-    locale === "ar"
-      ? item.font("Noto Naskh Arabic", "/fonts/NotoNaskhArabic-VariableFont_wght.ttf")
-      : null,
-  ].filter((i) => !!i)
-}
-
-function append(parcel) {
-  document.querySelectorAll(".postman").forEach((el) => el.remove())
-  parcel.json.forEach((json) => document.body.appendChild(json))
-  parcel.fonts.forEach((font) => document.fonts.add(font))
-  parcel.scripts.forEach((script) => document.body.appendChild(script))
-}
