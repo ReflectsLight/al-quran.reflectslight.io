ae769cc8f7463d683d41cee81ac267f149edbcd5
0x1eef <0x1eef@protonmail.com>
2025-04-11 19:28:30 -0300

    rake format:apply

--- src/js/components/App.tsx
+++ src/js/components/App.tsx
@@ -1,10 +1,10 @@
 import type { ReactNode } from "react"
 import { useTheme } from "~/hooks/useTheme"
-import type { Theme } from "~/hooks/useTheme";
+import type { Theme } from "~/hooks/useTheme"
 
 type AppContextType = {
-  theme: string;
-  setTheme: (theme: Theme) => void;
+  theme: string
+  setTheme: (theme: Theme) => void
 }
 
 export const AppContext = createContext<AppContextType>({} as AppContextType)
@@ -12,9 +12,7 @@ export const AppContext = createContext<AppContextType>({} as AppContextType)
 export function App({ children }: { children: ReactNode }) {
   const [theme, setTheme] = useTheme()
   return (
-    // @ts-ignore
-    <AppContext.Provider value={{ theme, setTheme }}>
-      {children}
-    </AppContext.Provider>
+    // @ts-expect-error invalid type for children
+    <AppContext.Provider value={{ theme, setTheme }}>{children}</AppContext.Provider>
   )
 }
